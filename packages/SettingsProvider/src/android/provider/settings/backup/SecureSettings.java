ef48a3652d54ea14d3b0dba68cdab5818b115a41
Sam Mortimer <sam@mortimer.me.uk>
2022-01-03 07:33:33 +0800

    fw/b: Add capability to allow tethering to use VPN upstreams [1/3]
    Squash of:
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Thu Aug 15 19:40:55 2019 -0700
    
        fw/b: Add capability to allow tethering to use VPN upstreams
    
        * Toggled on/off at runtime via a new hotspot lineage setting.
    
        * Dynamically updates the tethering upstream for existing hotspot
          clients as VPNs are brought up / down or the hotspot setting
          is changed.
    
        * This implementation depends on fw/b config_tether_upstream_automatic
          being set to true.
    
        Change-Id: I2ac0b4acc0ea686dfdf54561cb3428808e337160
    
    Change-Id: I2363e9baa007eac25be04ce23f0de41b80c5da42
    
    ----
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Fri Sep 13 16:27:37 2019 -0700
    
        fw/b: Prevent trying to enable hw offload for tethering via VPN upstreams
    
        * Tethering via VPN upstream requires a sw path.
    
        * hw offload setup happened to be being disabled anyway owing to a fail
          return code from setDataLimit().  However, it was causing offload to be
          disabled entirely (until next hotspot off / on event).
    
        * Gracefully skip hw offload for vpn upstreams so that it is automatically
          used again when a vpn is disconnected.
    
        Change-Id: I4df13f02889305560903b7b1e919eedc7af78c07
    
    [mkbestas]: Remove LineageSettings dependency & port to android 11
    
    [uldiniad]: Replaced Settings.Secure private APIs and variables with alternatives
    
    [Android 12]: Tethering moved to packages/modules/Connectivity
    
    Change-Id: I77ed0142e653f4993486eea44e4dac21e3f67f17

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -243,5 +243,7 @@ public class SecureSettings {
         Settings.Secure.ACCESSIBILITY_FONT_SCALING_HAS_BEEN_CHANGED,
         Settings.Secure.SEARCH_PRESS_HOLD_NAV_HANDLE_ENABLED,
         Settings.Secure.SEARCH_LONG_PRESS_HOME_ENABLED
+        Settings.Secure.ACCESSIBILITY_FONT_SCALING_HAS_BEEN_CHANGED,
+        Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS
     };
 }
